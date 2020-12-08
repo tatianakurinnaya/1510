44032ba20931814a9f218f0593d247e3a708b24b
tatianakurinnaya <75634063+tatianakurinnaya@users.noreply.github.com>
2020-12-08 15:09:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,16 +8,40 @@
 // @grant        none
 // ==/UserScript==
 
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min)+min);
+}
 let yandexInput = document.getElementsByName("text")[0];
 let btnY = document.getElementsByClassName("button mini-suggest__button button_theme_websearch button_size_ws-head i-bem button_js_inited")[0];
-yandexInput.value = "Гобой";
-btnY.click();
+let words = ["Гобой","Флейта","Как звучит флейта","Балалайка","Фагот","Скрипка","Саксофон"];
+let word = words[getRandom(0,words.length)];
+if (btnY!=undefined){
+    let i = 0;
+    let timerId = setInterval(function(){
+        yandexInput.value = yandexInput.value + word[i];
+        i++;
+        if(i == word.length){
+            clearInterval(timerId);
+            btnY.click();
+        }
+    },1000);
 
-let links = document.links;
-for (let i=0; i<links.length; i++){
-    let link = links[i];
-    if(link.href.includes("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")){
-        link.click();
-        break;
+}else{
+    let linkIsFound = false;
+    let links = document.links;
+    for (let i=0; i<links.length; i++){
+        let link = links[i];
+        if(link.href.includes("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")){
+            setTimeout(()=>{link.click();},1000);
+            linkIsFound = true;
+            break;
+        }
+    }
+    let pageNum = document.getElementsByClassName("pager__item pager__item_current_yes pager__item_kind_page")[0].innerText;
+    let nextPage = document.getElementsByClassName("pager__item_kind_next")[0];
+    if(!linkIsFound && pageNum<10){
+        setTimeout(()=>{nextPage.click();},1000);
+    }else{
+        location.href = "https://yandex.ru/";
     }
 }
