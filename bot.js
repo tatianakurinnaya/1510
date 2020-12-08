1be5feeb718ab8ea4e30b92edffb1536e868df50
tatianakurinnaya <75634063+tatianakurinnaya@users.noreply.github.com>
2020-12-08 19:38:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
 }
 let yandexInput = document.getElementsByName("text")[0];
-let btnY = document.getElementsByClassName("button mini-suggest__button button_theme_websearch button_size_ws-head i-bem button_js_inited")[0];
+let btnY = document.getElementsByClassName("button mini-suggest__button button_theme_websearch")[0];
 let words = ["Гобой","Флейта","Как звучит флейта","Балалайка","Фагот","Скрипка","Саксофон"];
 let word = words[getRandom(0,words.length)];
 if (btnY!=undefined){
@@ -32,16 +32,20 @@ if (btnY!=undefined){
     for (let i=0; i<links.length; i++){
         let link = links[i];
         if(link.href.includes("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")){
+            link.removeAttribute("target");
             setTimeout(()=>{link.click();},1000);
             linkIsFound = true;
             break;
         }
     }
-    let pageNum = document.getElementsByClassName("pager__item pager__item_current_yes pager__item_kind_page")[0].innerText;
-    let nextPage = document.getElementsByClassName("pager__item_kind_next")[0];
-    if(!linkIsFound && pageNum<10){
-        setTimeout(()=>{nextPage.click();},1000);
-    }else{
-        location.href = "https://yandex.ru/";
-    }
+
+  setTimeout(()=>{
+        let pageNum = document.querySelector("span.pager__item").innerText;
+        let nextPage = document.getElementsByClassName("pager__item_kind_next")[0];
+        if(!linkIsFound && pageNum<10){
+            setTimeout(()=>{nextPage.click();},1000);
+        }else{
+            location.href = "https://yandex.ru/";
+        }
+    },1000);
 }
